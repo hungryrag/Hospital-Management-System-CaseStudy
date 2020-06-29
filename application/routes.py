a54e3b898ba3d8bf986edbdfd807a9c26f26abab
hungryrag <supriyojana6@gmail.com>
2020-06-29 19:13:39 +0530

    add-diagnostic page

--- application/routes.py
+++ application/routes.py
@@ -35,4 +35,54 @@ def viewPatient():
 
 @app.route("/add-diagnostics", methods=["GET", "POST"])
 def addDiagonostics():
-    return render_template("add-diagnostics.html")
+    if request.method == "POST":
+        patient_id = request.form.get("patient_id")
+        patient = Patient.query.filter_by(ws_pat_id=patient_id).first()
+
+        diagnostic = Diagnostic.query.all()
+        diags = {}
+        # cost = []
+        for diag in patient.diagnostics:
+            diags[
+                diag.diagnostic_issued.ws_diagn
+            ] = diag.diagnostic_issued.ws_test_charge
+
+        # print(diags)
+
+        if patient:
+            return render_template(
+                "add-diagnostics.html",
+                title="Diagnostics",
+                patient=patient,
+                diagnostic=diagnostic,
+                diag_issued=diags,
+            )
+        else:
+            flash("Patient ID does not exist. Enter corrent Patient ID", "danger")
+            return render_template(
+                "add-diagnostics.html",
+                title="Diagnostics",
+                patient=None,
+                diagnostic=None,
+                diag_issued=None,
+            )
+    else:
+        return render_template(
+            "add-diagnostics.html",
+            title="Diagnostics",
+            patient=None,
+            diagnostic=None,
+            diag_issued=None,
+        )
+
+
+@app.route("/update-diagnostics", methods=["GET", "POST"])
+@app.route("/update-diagnostics/<patient_id>", methods=["GET", "POST"])
+def updateDiagnostics(patient_id):
+    patient_id = request.args.get("patient_id")
+    print(patient_id)
+    patient = Patient.query.filter_by(ws_pat_id=patient_id).first()
+    return render_template(
+        "update-diagnostics.html", title="Diagnostics", patient=patient
+    )
+
